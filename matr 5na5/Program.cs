57ac0193cdc77f9e94e4f675c6cb93722d7ff272
daniil davtyan <daniildavtyan@outlook.com>
2020-05-28 13:37:45 +0700

    добавлене проект

---
+++ matr 5na5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matr_5na5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
